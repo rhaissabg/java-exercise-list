bbbf4bc72a7237815810e8a58b50aedcb3103e20
Rhaíssa Lima <rhaissasbgl@gmail.com>
2025-07-24 15:23:50 -0400

    nineteenth and twentieth exercises

---
+++ MultiplicationTableGenerator.java
@@ -0,0 +1,36 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class MultiplicationTableGenerator {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number to generate it's table: ");
+		int number = sc.nextInt();
+		
+		System.out.print("Enter the start number: ");
+		int start = sc.nextInt();
+		
+		System.out.print("Enter the end number: ");
+		int end = sc.nextInt();
+		
+		if (start < end) {
+			for (int i = start; i <= end; i++) {
+				System.out.printf("%d x %d = %d%n", number, i, (number * i));
+			}
+		} else if (start > end) {
+			for (int i = start; i >= end; i--) {
+				System.out.printf("%d x %d = %d%n", number, i, (number * i));
+			}
+		} else {
+			System.out.printf("%d x %d = %d", number, start, (number * start));
+		}
+		
+		sc.close();
+		
+	}
+
+}
